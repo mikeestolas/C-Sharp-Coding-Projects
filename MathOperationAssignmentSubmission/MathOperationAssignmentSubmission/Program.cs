a9143523f17191221f4c15745a83c21e6dacddf7
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-07-27 18:38:51 -0400

    .

--- MathOperationAssignmentSubmission/MathOperationAssignmentSubmission/Program.cs
+++ MathOperationAssignmentSubmission/MathOperationAssignmentSubmission/Program.cs
@@ -24,17 +24,17 @@ namespace MathOperationAssignmentSubmission
             //2.
             Console.WriteLine("Please type any number from 1 - 100: ");
             String numFromUser2 = Console.ReadLine();
-            int num3 = Convert.ToInt32(numFromUser);
+            int num3 = Convert.ToInt32(numFromUser2);
             int num4 = 25;
             int Total1 = num3 + num4;
-            Console.WriteLine("The total of your number when Added to 25 is equal to:" + Total);
+            Console.WriteLine("The total of your number when Added to 25 is equal to:" + Total1);
             Console.ReadLine();
 
 
             //3.
             Console.WriteLine("Please type any decimal number from 1 - 100: ");
             string numFromUser3 = Console.ReadLine();
-            float num5 = float.Parse(numFromUser);
+            float num5 = float.Parse(numFromUser3);
             double num6 = 12.5;
             Double Total2 = num5 / num6;
             Console.WriteLine("The total of your number when divided to 12.5 is equal to:" + Total2);
@@ -44,7 +44,7 @@ namespace MathOperationAssignmentSubmission
             //4.
             Console.WriteLine("Please type any number from 1 - 100: ");
             String numFromUser4 = Console.ReadLine();
-            int num7 = Convert.ToInt32(numFromUser);
+            int num7 = Convert.ToInt32(numFromUser4);
             int num8 = 50;
             bool Result = num7 > num8;
             Console.WriteLine("Is your number Grater than 50?:" + Result);
@@ -54,7 +54,7 @@ namespace MathOperationAssignmentSubmission
             //5.
             Console.WriteLine("Please type any number from 1 - 100: ");
             String numFromUser5 = Console.ReadLine();
-            int num9 = Convert.ToInt32(numFromUser);
+            int num9 = Convert.ToInt32(numFromUser5);
             int num10 = 7;
             int remainder = num9 % num10;
             Console.WriteLine("your number devided to 7 has a remainder of:" + remainder);
