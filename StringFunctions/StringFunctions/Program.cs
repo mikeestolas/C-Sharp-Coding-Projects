3c9b381362bcaa4755bcbdc5ada1844a75011af4
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-07-27 18:11:33 -0400

    .

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string firstName = "Michael ";
+            String middle = "Payba";
+            string lastName = "Estolas";
+            string name = firstName + middle + lastName;
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+
+            String name1 = "michael estolas";
+            name1 = name1.ToUpper();
+            Console.WriteLine(name1);
+            Console.ReadLine();
+
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("The quick brown fox just sat over the lazy dog!");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
