0ab518acd4da27aecf15aad59287ec6ffad22c2e
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-08-12 22:16:16 -0400

    .

---
+++ ConsoleAppAssignmentPart6/ConsoleAppAssignmentPart6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleAppAssignmentPart6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruit = new List<string>() { "Apple", "Banana", "Mango", "Orange", "Grapes", "Apple" };
+            List<string> appeared = new List<string>();
+
+
+            foreach (string fruits in fruit)
+            {
+                if (fruits == "Apple")
+                {
+                    appeared.Add(fruits);
+
+                    Console.WriteLine(fruits);
+                    Console.WriteLine("There are " + (appeared.Count) + " in the list!");
+
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
