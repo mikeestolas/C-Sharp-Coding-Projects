c359d874647a65a2867c5fec249111c9c4da5e74
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-08-24 17:29:57 -0400

    .

---
+++ ClassesandMethodsAssignment2/ClassesandMethodsAssignment2/NumberManipulator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment2
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+            return result;
+        }
+    }
+}
---
+++ ClassesandMethodsAssignment2/ClassesandMethodsAssignment2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Please Enter first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter second number:");
+
+            //int a = 100;
+            //int b = 200;
+            int ret;
+            NumberManipulator n = new NumberManipulator();
+
+            
+            ret = n.FindMax(a, b);
+            Console.WriteLine("Max value is : {0}", ret);
+            Console.ReadLine();
+        }
+    }
+}
