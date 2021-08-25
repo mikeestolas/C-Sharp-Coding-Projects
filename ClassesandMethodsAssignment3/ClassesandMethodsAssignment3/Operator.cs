a5acb18997f87eb3cd3e111ad03726c0e7c8936c
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-08-24 21:35:10 -0400

    .

---
+++ ClassesandMethodsAssignment3/ClassesandMethodsAssignment3/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment3
+{
+    class Operator
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+    }
+}
---
+++ ClassesandMethodsAssignment3/ClassesandMethodsAssignment3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\nFunction to calculate the sum of two numbers :\n");
+            Console.Write("--------------------------------------------------\n");
+            Console.Write("Enter a number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Operator.Sum(n1, n2));
+            Console.ReadLine();
+        }
+
+        
+    }
+}
