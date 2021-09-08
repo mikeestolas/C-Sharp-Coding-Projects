53bcaf2bb7d0dede9a4cd497b7d45ad59f0dea95
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-09-07 22:22:53 -0400

    .

--- ClassesandMethodsAssignment3/ClassesandMethodsAssignment3/Program.cs
+++ ClassesandMethodsAssignment3/ClassesandMethodsAssignment3/Program.cs
@@ -8,18 +8,28 @@ namespace ClassesandMethodsAssignment3
 {
     class Program
     {
+        static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+
         static void Main(string[] args)
         {
+
             Console.Write("\n\nFunction to calculate the sum of two numbers :\n");
             Console.Write("--------------------------------------------------\n");
             Console.Write("Enter a number: ");
             int n1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter another number: ");
             int n2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Operator.Sum(n1, n2));
+            int z = Sum(n1,100);
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", z);
+            Console.WriteLine("Second number is = " + n2);
             Console.ReadLine();
         }
-
+        
         
     }
 }
