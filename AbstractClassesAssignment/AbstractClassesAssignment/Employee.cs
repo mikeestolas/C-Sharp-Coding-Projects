91a38665d0e1d68a366e733354d52a6a96a5d96d
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-09-13 16:23:37 -0400

    .

--- AbstractClassesAssignment/AbstractClassesAssignment/Employee.cs
+++ AbstractClassesAssignment/AbstractClassesAssignment/Employee.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 
 namespace AbstractClassesAssignment
 {
+    
     class Employee : Person
     {
         public static int id = 1212;
+        string FirstName = "Sample";
+        string LastName = "Student";
         public override void SayName()
         {
 
             base.SayName();
             Console.WriteLine("Employee ID: {0}", id);
-            //Console.WriteLine("FirstName: {0}", FirstName);
-            //Console.WriteLine("LastName: {0}", LastName);
+            Console.WriteLine("FirstName: {0}", FirstName);
+            Console.WriteLine("LastName: {0}", LastName);
             Console.ReadLine();
 
         }
