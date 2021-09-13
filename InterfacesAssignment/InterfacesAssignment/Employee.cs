9caacedb4d122dd69e79316ab75e52f6566dea22
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-09-13 19:25:17 -0400

    .

---
+++ InterfacesAssignment/InterfacesAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    class Employee : Person, IQuittable
+    {
+        public static int id = 1212;
+        string FirstName = "Sample";
+        string LastName = "Student";
+        public override void SayName()
+        {
+
+            base.SayName();
+            Console.WriteLine("Employee ID: {0}", id);
+            Console.WriteLine("FirstName: {0}", FirstName);
+            Console.WriteLine("LastName: {0}", LastName);
+            Console.ReadLine();
+        }
+        public void Quit(Employee employee)
+        {
+            throw new NotFiniteNumberException();
+        }
+    }
+}
