0d47dc3075fb7d1cc13eb7a321389ab5c64f6996
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-09-27 21:54:19 -0400

    .

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter a number:");
+            string userIn = Convert.ToString(Console.ReadLine());
+           
+
+
+            File.WriteAllText(@"C:\Users\Admin\OneDrive - Sysco Corporation\Desktop\Logs\log.txt", userIn);
+
+            Console.WriteLine("You have enter " + userIn);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
