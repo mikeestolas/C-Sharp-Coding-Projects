f3f580df74827002dea0cee4b0baef25074c962b
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-10-25 19:44:01 -0400

    .

---
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Please type your Age :");
+                int xAge = Convert.ToInt32(Console.ReadLine());
+
+                var age = DateTime.Today.Year - xAge;
+
+                if (xAge <= 0) throw new("Number must be positive and non zero");
+                    
+
+                Console.WriteLine("you were born on year : " + age);
+            }
+            
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message + "\nYour have entered Invalid number");
+               
+            }
+            
+
+
+
+        }
+    }
+}
+    
+
