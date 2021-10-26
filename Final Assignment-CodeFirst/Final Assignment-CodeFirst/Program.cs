f0ced569da1a985d1928978ab92600bf844224a6
mikeestolas <81533880+mikeestolas@users.noreply.github.com>
2021-10-26 19:19:24 -0400

    .

---
+++ Final Assignment-CodeFirst/Final Assignment-CodeFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Assignment_CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Michael" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            Console.ReadLine();
+        }
+    }
+}
